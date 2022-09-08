559feff456a458086317deb368bff960387b7242
EmilianoA-B <emi.ariza.b@gmail.com>
2022-09-08 08:35:16 -0500

    First commit

--- ProyectoED.cpp
+++ ProyectoED.cpp
@@ -8,12 +8,13 @@ struct pan{
     string nombre;
 };
 
+void menu();
 int incializarArreglo(struct pan datos[]);
 void agregarDatos(struct pan datos[]);
 void ordenarArreglo(struct pan datos[]);
 void buscar(struct pan datos[]);
-
 void imprimir(struct pan datos[]);
+
 int main(){
     int ultArr;
     pan datos[50];
@@ -45,30 +46,26 @@ int main(){
     }
 }
 
+void menu(){
+    cout<<"Panaderia\n\n"<<endl;
+}
+
 int incializarArreglo(struct pan datos[]){    
     string nombres[10] = {"Concha", "Dona", "Bolillo","Cuernito","Oreja","Pan de muerto"}; 
-    
-    for (int i = 0; i <= 10; i++)
+    int i;
+    for (i = 0; i <= 10; i++)
     {
         datos[i].precio = 1 + rand()%20;
-    }
-    
-    for (int i = 0; i <= 10; i++)
-    {
         datos[i].cantidad = 1+rand()%80;
-    }
-    
-    for(int i=0 ;i<10;i++){
         datos[i].nombre = nombres[i];
     }
+    return i;
 }
 
 void imprimir(struct pan datos[]){
-
     for(int i = 0; i < 10; i++){
         cout << "Nombre: " << datos[i].nombre << " ";
         cout << "Existencia: " << datos[i].cantidad <<" ";
         cout << "Precio: " << datos[i].precio << " " <<endl;
     }
-
 }
