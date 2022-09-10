7462cc9d971a5b5d0980ad268c5dd13c597965c4
iSgRuby <manuel_31oct@outlook.com>
2022-09-09 23:57:11 -0500

    metodo de ordenamient falta solucionar problemas con el metodo de ordenamiento dentro del switch.

--- ProyectoED.cpp
+++ ProyectoED.cpp
@@ -12,11 +12,12 @@ struct pan{
 void menu();
 int incializarArreglo(struct pan datos[]);
 void agregarDatos(struct pan datos[]);
-void ordenarArreglo(struct pan datos[]);
 void imprimir(struct pan datos[], int lp);
-void ordenar(struct pan datos[], int lp);
+void menuOrdenar(int opc, struct pan datos[], int lp);
 void buscar(struct pan datos[], int lp);
 int agrega(struct pan datos[], int lp);
+void insersionBinaria(int, struct pan datos[], int lp);
+void InsBinCant(int, struct pan datos[], int lp);
 
 int main(){
     int ultArr;
@@ -37,7 +38,7 @@ int main(){
             break;
 
             case 3: //Ordenar
-
+                menuOrdenar(opc, datos, ultArr);
             break;
 
             case 4: //Buscar
@@ -54,13 +55,93 @@ int main(){
     }
 }
 
-void menuOrdenar(){
+void menuOrdenar(int opc, struct pan datos[], int lp){
     cout<<"Ordenar:\n\n"<<
         "\t1. Alfabeticamente\n"<<
         "\t2. Por existencia\n"<<
         "\t3. Por precio (Mayor a menor)\n"<<
-        "\t4. Por precio (Menos a mayor)\n"<<endl;
+        "\t4. Por precio (Menos a mayor)\n\n"<<
+        "0. Salir\n\n Selecciona la opcion: ";
+    cin >> opc;
+   while (opc > 4)
+   {
+        cout << "Selecciona una opcion correcta: ";
+        cin >> opc;
+   }
+   
+    
+    insersionBinaria(opc, datos, lp);
 }
+
+void insersionBinaria(int opc, struct pan datos[], int lp){
+	
+	string minN, auxN;
+        int auxC, minC, auxP, minP;
+        int pos;
+        cout << lp;
+        for(int i = 0; i < lp; i++)
+        {
+            minN = datos[i].nombre;
+            auxN = datos[i].nombre;
+            minC = datos[i].cantidad;
+            auxC = datos[i].cantidad;
+            minP = datos[i].precio;
+            minP = datos[i].precio;
+            for(int j = i + 1; j <= lp; j ++)
+            {
+                switch (opc)
+                {
+                case 1:
+                    if (datos[j].nombre < minN)
+                    {
+                        minN = datos[j].nombre;
+                        minC = datos[j].cantidad;
+                        minP = datos[j].precio;
+                        pos = j;
+                    }else{
+                        break;
+                    }
+                    
+                case 2:
+                    if (datos[j].cantidad > minC)
+                    {
+                        minN = datos[j].nombre;
+                        minC = datos[j].cantidad;
+                        minP = datos[j].precio;
+                        pos = j;
+                    }
+                    break;
+                case 3:
+                    if (datos[j].precio > minP)
+                    {
+                        minN = datos[j].nombre;
+                        minC = datos[j].cantidad;
+                        minP = datos[j].precio;
+                        pos = j;
+                    }
+                    break;
+                case 4:
+                    if (datos[j].precio < minP)
+                    {
+                        minN = datos[j].nombre;
+                        minC = datos[j].cantidad;
+                        minP = datos[j].precio;
+                        pos = j;
+                    }
+                    break;
+                }
+            }
+            datos[pos].nombre = auxN;
+            datos[i].nombre = minN;
+            datos[pos].cantidad = auxC;
+            datos[i].cantidad = minC;
+            datos[pos].precio = auxP;
+            datos[i].precio = minP;
+            
+        }
+        cout << "Inventario ordenado por nombre\n\n";
+}
+
 
 void menu(){
     cout<<"Panaderia\n\n"<<
@@ -110,7 +191,7 @@ int agrega(struct pan datos[], int lp){
 
 void imprimir(struct pan datos[], int lp){
     for(int i = 0; i <lp; i++){
-        cout << "Nombre:    " << datos[i].nombre << "|";
+        cout << "[" << i + 1 << "]  " << "\tNombre:    " << datos[i].nombre << "|";
         cout << "Existencia:    " << datos[i].cantidad <<"\t| ";
         cout << "Precio:    " << datos[i].precio << "\t|" <<endl;
     }
